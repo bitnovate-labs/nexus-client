b2d16d703e1f6b58c4d1785b39deb1cb6fd73e44
timlimytn <contactus@pixlsquares.com>
2024-12-19 16:47:07 +0800

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
+// Use import.meta.url to get the directory name in ES modules
+const __dirname = new URL(".", import.meta.url).pathname;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+    },
   },
 });
