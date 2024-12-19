07d3793d2030b1956ba59ec021c2b3659144d097
timlimytn <contactus@pixlsquares.com>
2024-12-19 18:10:36 +0800

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import alias from "@rollup/plugin-alias";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    alias({
+      entries: [{ find: "components", replacement: "/src/components" }],
+    }),
+  ],
   css: {
     postcss: "./postcss.config.js",
+  },
+  resolve: {
+    alias: {
+      // Alias for utils folder
+      components: "/src/components",
+    },
   },
 });
