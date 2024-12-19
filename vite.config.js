8591c146b7c7673293a09c3c92011427faadd6db
timlimytn <contactus@pixlsquares.com>
2024-12-20 07:04:22 +0800

    udpated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "log-resolve",
+      resolveId(source) {
+        console.log("Resolving:", source);
+        return null; // Proceed with default resolution
+      },
+    },
+  ],
   css: {
     postcss: "./postcss.config.js",
   },
