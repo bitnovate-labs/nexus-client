0acfe22a4904cf9dd8578a42d8c4870bc435cb40
timlimytn <contactus@pixlsquares.com>
2024-12-20 08:12:13 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+// import path from "path";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,12 +13,12 @@ export default defineConfig({
     extensions: [".js", ".jsx"], // Ensure .jsx is included
     alias: {
       // Define an alias for the 'components' directory
-      "@components": path.resolve(__dirname, "src/components"),
+      // "@components": path.resolve(__dirname, "src/components"),
+      "@components": fileURLToPath(new URL("./src", import.meta.url)),
       // Define an alias for the 'pages' directory (if you have one)
-      "@graphql": path.resolve(__dirname, "src/graphql"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@pages": path.resolve(__dirname, "src/pages"),
+      // "@graphql": path.resolve(__dirname, "src/graphql"),
+      // "@hooks": path.resolve(__dirname, "src/hooks"),
+      // "@pages": path.resolve(__dirname, "src/pages"),
     },
   },
 });
-console.log("Alias @components:", path.resolve(__dirname, "src/components"));
